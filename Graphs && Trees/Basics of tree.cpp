ed6a08949fb774194a1d60d0be655b465218a0f4
حسان احمد <139946422+pforprogrammer@users.noreply.github.com>
2023-09-15 20:07:36 +0500

    Basics of tree.cpp

--- Graphs && Trees/Basics of tree.cpp
+++ Graphs && Trees/Basics of tree.cpp
@@ -1,4 +1,73 @@
-// Online C++ compiler to run C++ program online
+//creates nodes of tree and travers in pre , in & post order
+#include <iostream>
+using namespace std;
+    class node{
+        public:
+        int data;
+        node* left;
+        node* right;
+        node(int val){
+            this->data = val;
+            this->left = NULL;
+            this->right = NULL;
+        }
+    };
+    void preorder(node* root){
+        
+        if(root == NULL){
+            return;
+        }
+        cout<<root->data<<" , ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+    void inorder(node* root){
+        if(root == NULL){
+            return;
+        }
+        inorder(root->left);
+        cout<<root->data<<" , ";
+        inorder(root->right);
+    }
+    void postorder(node* root){
+        if(root == NULL){
+            return;
+        }
+        
+        postorder(root->left);
+        postorder(root->right);
+        cout<<root->data<<" , ";    
+        
+    }
+int main() {
+     node*  root = new node(1);
+    root->left =    new node(2);
+    root->right = new node(3);
+    root->left->left =    new node(4);
+    root->left->right =    new node(5);
+    root->right->left = new node(9);
+    root->right->right = new node(10);
+    cout<<"preorder of Tree is :"<<endl;
+    preorder(root);
+    cout<<endl<<"inorder of Tree is :"<<endl;
+    inorder(root);
+    cout<<endl<<"postorder of Tree is :"<<endl;
+    postorder(root);
+    return 0;
+}
+// output is :
+// preorder of Tree is :
+// 1 , 2 , 4 , 5 , 3 , 9 , 10 , 
+// inorder of Tree is :
+// 4 , 2 , 5 , 1 , 9 , 3 , 10 , 
+// postorder of Tree is :
+// 4 , 5 , 2 , 9 , 10 , 3 , 1 , 
+
+
+
+
+
+//Insertion all nodes of tree
 #include <iostream>
 using namespace std;
     class node{
